e03d0a495eb868eb3ad4b00c2002696d811cc5f3
Kata Gáspár <kata.gaspar2@stud.uni-corvinus.hu>
2020-12-15 10:53:22 +0100

    Commit properties

---
+++ FinalVersion/Entities/Adat.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalVersion.Entities
+{
+    public class Adat
+    {
+        public string Telepules { get; set; }
+        public string Hotelnev { get; set; }
+        public string Sorszam { get; set; }
+        public int Ejszaka { get; set; }
+        public int Forint { get; set; }
+    }
+}
